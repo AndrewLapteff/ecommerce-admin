9e2e56d69ce241400e878fcf4630536574369e78
Andrew Lapteff <this.is.amigogo@gmail.com>
2023-12-20 13:20:00 +0200

    Fixed bug

--- app/api/file/route.ts
+++ app/api/file/route.ts
@@ -1,21 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import fs from 'fs'
-import * as path from 'path'
 import { NextResponse } from 'next/server'
-import { Stream } from 'stream'
 
 export async function GET(request: NextApiRequest, response: NextApiResponse) {
-  if (request.method !== 'GET') {
-    return response.status(405).end()
-  }
+  // if (request.method !== 'GET') {
+  //   return response.status(405).end()
+  // }
 
-  const stream = new ReadableStream({
-    start(controller) {
-      const file = fs.readFileSync(path.join(process.cwd(), 'torrents', 'test.torrent'))
-      controller.enqueue(file)
-      controller.close()
-    }
-  })
+  // const stream = new ReadableStream({
+  //   start(controller) {
+  //     const file = fs.readFileSync(path.join(process.cwd(), 'torrents', 'test.torrent'))
+  //     controller.enqueue(file)
+  //     controller.close()
+  //   }
+  // })
 
-  return new NextResponse(stream)
+  return NextResponse.json({ message: 'Hello' })
 }
